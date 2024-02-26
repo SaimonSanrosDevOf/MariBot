beae071fc2b97ee8a5388d0df525ba3f34f17ba5
SaimonSanrosDevOf <161264671+SaimonSanrosDevOf@users.noreply.github.com>
2024-02-25 22:20:12 -0300

    Create config.py.

---
+++ config.py
@@ -0,0 +1,7 @@
+# config.py
+
+# Token do seu bot do Telegram
+TELEGRAM_TOKEN = "6882824674:AAFm8MFoQ12lnxA22oBrqepjFcLlcB_mv_g"
+
+# Chave de API do Google
+GOOGLE_API_KEY = "AIzaSyArng8NvRItT8BKsMHMcdUD0TNxS-hW9Bw"
